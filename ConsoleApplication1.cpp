319545b1f78ea807c6daebebea7bdc9eaa4f6774
chertousovapolina <115408269+chertousovapolina@users.noreply.github.com>
2022-10-09 16:39:32 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,15 @@
+﻿// Chertousova PE
+//  Task 1
+// 9.10.2022
+#include <iostream>
+using namespace std; 
+int main()
+{
+	int A;
+	cin >> A;
+	int fisrt_digit, second_digit;//fd,sd
+	second_digit = A % 10;
+    cout << second_digit;
+	second_digit *= 10;
+
+}
